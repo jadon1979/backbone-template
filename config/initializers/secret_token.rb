2b7ab9efcc63e34710bc4eb3910b8dec9deeaf15
Chapley Watson <jadon1979@yahoo.com>
2011-10-24 03:03:21 -0500

    Init Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bonetest::Application.config.secret_token = '0e332c00912cfcc08ecc034ec0704894ad0962a1ccf44a1c40d61534dd2f202e45393115117c9c51fadb2bcb9245013e3b334a97d14840fcddce681edb3aae1a'
